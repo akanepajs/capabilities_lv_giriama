f3b7fadbb56211614a37811b89af27866f264643
akanepajs <40496475+akanepajs@users.noreply.github.com>
2024-10-11 02:29:00 +0300

    Update capabilities_lv.py

--- capabilities_lv.py
+++ capabilities_lv.py
@@ -13,19 +13,35 @@ import os
 os.environ['ANTHROPIC_API_KEY'] = userdata.get('ANTHROPIC_API_KEY')
 os.environ['GOOGLE_API_KEY'] = userdata.get('GOOGLE_API_KEY')
 os.environ['OPENAI_API_KEY'] = userdata.get('OPENAI_API_KEY')
+os.environ['TOGETHER_API_KEY'] = userdata.get('TOGETHER_API_KEY')
+os.environ['MISTRAL_API_KEY'] = userdata.get('MISTRAL_API_KEY')
 
 #models
 # https://inspect.ai-safety-institute.org.uk/
 # https://docs.anthropic.com/en/docs/about-claude/models 
 # https://ai.google.dev/gemini-api/docs/models/gemini 
+# https://docs.mistral.ai/getting-started/models/models_overview/
+# https://api.together.ai/models 
+
 
 # Run the inspect command
+#!inspect eval mmlu.py --model openai/gpt-4o-2024-08-06	--limit 50
+#!inspect eval mmlu.py --model anthropic/claude-3-5-sonnet-20240620	--limit 50
+#!inspect eval mmlu.py --model google/gemini-1.5-pro-002 --limit 50
+#!inspect eval mmlu.py --model together/meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo
+#!inspect eval mmlu.py --model mistral/mistral-large-latest
+
+#!inspect eval mmlu_lv.py --model openai/gpt-4o-2024-08-06	--limit 50
+#!inspect eval mmlu_lv.py --model anthropic/claude-3-5-sonnet-20240620	--limit 50
+#!inspect eval mmlu_lv.py --model google/gemini-1.5-pro-002 --limit 50
+#!inspect eval mmlu_lv.py --model together/meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo
+#!inspect eval mmlu_lv.py --model mistral/mistral-large-latest
+
+# Other models
 #!inspect eval mmlu_lv.py --model openai/o1-mini-2024-09-12		--limit 50
 #!inspect eval mmlu_lv.py --model openai/o1-preview-2024-09-12 --limit 50
 #!inspect eval mmlu_lv.py --model anthropic/claude-3-haiku-20240307 --limit 50
-#!inspect eval mmlu_lv.py --model anthropic/claude-3-5-sonnet-20240620	--limit 50
 #!inspect eval mmlu_lv.py --model google/gemini-1.5-flash-002	--limit 50
-#!inspect eval mmlu_lv.py --model google/gemini-1.5-pro-002 --limit 50
 #!inspect eval mmlu_lv.py --model openai/gpt-4o-mini-2024-07-18	--limit 50
-#!inspect eval mmlu_lv.py --model openai/gpt-4o-2024-08-06	--limit 50
-!inspect eval mmlu_lv.py --model openai/gpt-4o-realtime-preview-2024-10-01	--limit 50
+#!inspect eval mmlu_lv.py --model openai/gpt-4o-realtime-preview-2024-10-01	--limit 50
+
